485c6fcfce4be12b37fefc17cb818ac8fb6b18ea
jimenaseitz <113646608+jimenaseitz@users.noreply.github.com>
2022-11-13 19:14:54 -0300

    sql y proyecto

---
+++ DB/nutricionista.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-11-2022 a las 23:08:17
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `nutricionista`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comida`
+--
+
+CREATE TABLE `comida` (
+  `idComida` int(11) NOT NULL,
+  `nombre` int(50) NOT NULL,
+  `detalle` varchar(200) NOT NULL,
+  `calorias` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dieta`
+--
+
+CREATE TABLE `dieta` (
+  `idDieta` int(11) NOT NULL,
+  `idPaciente` int(11) NOT NULL,
+  `inicioDieta` date NOT NULL,
+  `finDieta` date NOT NULL,
+  `pesoBuscado` double NOT NULL,
+  `limiteClorico` int(1) NOT NULL,
+  `pesoInicial` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `itemcomidas`
+--
+
+CREATE TABLE `itemcomidas` (
+  `idItemComida` int(11) NOT NULL,
+  `idDieta` int(11) NOT NULL,
+  `idComida` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `idPaciente` int(11) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `domicilio` varchar(100) NOT NULL,
+  `telefono` int(12) NOT NULL,
+  `altura` double NOT NULL,
+  `pesoActual` double NOT NULL,
+  `fechaNacimiento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `comida`
+--
+ALTER TABLE `comida`
+  ADD PRIMARY KEY (`idComida`);
+
+--
+-- Indices de la tabla `dieta`
+--
+ALTER TABLE `dieta`
+  ADD PRIMARY KEY (`idDieta`),
+  ADD KEY `idPaciente` (`idPaciente`);
+
+--
+-- Indices de la tabla `itemcomidas`
+--
+ALTER TABLE `itemcomidas`
+  ADD PRIMARY KEY (`idItemComida`),
+  ADD KEY `idDieta` (`idDieta`),
+  ADD KEY `idComida` (`idComida`);
+
+--
+-- Indices de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`idPaciente`,`dni`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comida`
+--
+ALTER TABLE `comida`
+  MODIFY `idComida` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `dieta`
+--
+ALTER TABLE `dieta`
+  MODIFY `idDieta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `itemcomidas`
+--
+ALTER TABLE `itemcomidas`
+  MODIFY `idItemComida` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comida`
+--
+ALTER TABLE `comida`
+  ADD CONSTRAINT `comida_ibfk_1` FOREIGN KEY (`idComida`) REFERENCES `itemcomidas` (`idComida`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `dieta`
+--
+ALTER TABLE `dieta`
+  ADD CONSTRAINT `dieta_ibfk_1` FOREIGN KEY (`idPaciente`) REFERENCES `paciente` (`idPaciente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `dieta_ibfk_2` FOREIGN KEY (`idDieta`) REFERENCES `itemcomidas` (`idDieta`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
