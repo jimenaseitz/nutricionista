b772f2ca214d2fb9edde4bb68196cbab6d624134
jimenaseitz <113646608+jimenaseitz@users.noreply.github.com>
2022-11-13 21:46:52 -0300

    Update nutricionista.sql

--- DB/nutricionista.sql
+++ DB/nutricionista.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-11-2022 a las 23:08:17
+-- Tiempo de generación: 14-11-2022 a las 01:45:49
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `nutricionista`
 --
+CREATE DATABASE IF NOT EXISTS `nutricionista` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `nutricionista`;
 
 -- --------------------------------------------------------
 
@@ -29,7 +31,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `comida` (
   `idComida` int(11) NOT NULL,
-  `nombre` int(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
   `detalle` varchar(200) NOT NULL,
   `calorias` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -46,7 +48,7 @@ CREATE TABLE `dieta` (
   `inicioDieta` date NOT NULL,
   `finDieta` date NOT NULL,
   `pesoBuscado` double NOT NULL,
-  `limiteClorico` int(1) NOT NULL,
+  `limiteCalorico` int(1) NOT NULL,
   `pesoInicial` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -109,7 +111,8 @@ ALTER TABLE `itemcomidas`
 -- Indices de la tabla `paciente`
 --
 ALTER TABLE `paciente`
-  ADD PRIMARY KEY (`idPaciente`,`dni`);
+  ADD PRIMARY KEY (`idPaciente`),
+  ADD UNIQUE KEY `dni` (`dni`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -132,6 +135,12 @@ ALTER TABLE `dieta`
 --
 ALTER TABLE `itemcomidas`
   MODIFY `idItemComida` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  MODIFY `idPaciente` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
