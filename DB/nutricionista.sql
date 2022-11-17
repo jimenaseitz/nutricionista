4cfb4942c69011ee940788ef366f5d3290dd44a0
pablojlpk <113646557+pablojlpk@users.noreply.github.com>
2022-11-17 19:35:41 -0300

    -

--- DB/nutricionista.sql
+++ DB/nutricionista.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-11-2022 a las 02:03:59
+-- Tiempo de generación: 15-11-2022 a las 00:03:27
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `nutricionista`
+-- Base de datos: `nutricionistaultimo`
 --
 
 -- --------------------------------------------------------
@@ -28,11 +28,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `comida` (
-  `idComida` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `detalle` varchar(200) NOT NULL,
-  `calorias` int(11) NOT NULL
+  `id_comida` int(11) NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `detalle` varchar(100) NOT NULL,
+  `calorias` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `comida`
+--
+
+INSERT INTO `comida` (`id_comida`, `nombre`, `detalle`, `calorias`, `estado`) VALUES
+(10, 'Pollo', 'parrilla', 100, 1),
+(12, 'Tomate', 'parrilla', 100, 1);
 
 -- --------------------------------------------------------
 
@@ -41,14 +50,21 @@ CREATE TABLE `comida` (
 --
 
 CREATE TABLE `dieta` (
-  `idDieta` int(11) NOT NULL,
-  `idPaciente` int(11) NOT NULL,
-  `inicioDieta` date NOT NULL,
-  `finDieta` date NOT NULL,
+  `id_dieta` int(11) NOT NULL,
+  `id_paciente` int(11) NOT NULL,
+  `iniciodieta` date NOT NULL,
+  `findieta` date NOT NULL,
   `pesoBuscado` double NOT NULL,
   `limiteCalorico` int(1) NOT NULL,
   `pesoInicial` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `dieta`
+--
+
+INSERT INTO `dieta` (`id_dieta`, `id_paciente`, `iniciodieta`, `findieta`, `pesoBuscado`, `limiteCalorico`, `pesoInicial`) VALUES
+(8, 4, '2022-11-09', '2022-11-08', 10.5, 22, 10.5);
 
 -- --------------------------------------------------------
 
@@ -57,17 +73,18 @@ CREATE TABLE `dieta` (
 --
 
 CREATE TABLE `itemcomidas` (
-  `idItemComida` int(11) NOT NULL,
-  `idDieta` int(11) NOT NULL,
-  `idComida` int(11) NOT NULL
+  `id_itemcomida` int(11) NOT NULL,
+  `id_dieta` int(11) NOT NULL,
+  `id_comida` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `itemcomidas`
 --
 
-INSERT INTO `itemcomidas` (`idItemComida`, `idDieta`, `idComida`) VALUES
-(1, 1, 1);
+INSERT INTO `itemcomidas` (`id_itemcomida`, `id_dieta`, `id_comida`) VALUES
+(7, 8, 10),
+(10, 8, 12);
 
 -- --------------------------------------------------------
 
@@ -76,7 +93,7 @@ INSERT INTO `itemcomidas` (`idItemComida`, `idDieta`, `idComida`) VALUES
 --
 
 CREATE TABLE `paciente` (
-  `idPaciente` int(11) NOT NULL,
+  `id_paciente` int(11) NOT NULL,
   `dni` int(11) NOT NULL,
   `apellido` varchar(50) NOT NULL,
   `nombre` varchar(50) NOT NULL,
@@ -84,17 +101,18 @@ CREATE TABLE `paciente` (
   `telefono` int(12) NOT NULL,
   `altura` double NOT NULL,
   `pesoActual` double NOT NULL,
-  `fechaNacimiento` date NOT NULL
+  `fechaNacimiento` date NOT NULL,
+  `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `paciente`
 --
 
-INSERT INTO `paciente` (`idPaciente`, `dni`, `apellido`, `nombre`, `domicilio`, `telefono`, `altura`, `pesoActual`, `fechaNacimiento`) VALUES
-(1, 34429045, 'Seitz', 'Jimena', 'villa del carmen', 358482879, 1.7, 71, '1990-02-07'),
-(2, 27377277, 'Pujato', 'Pablo', 'dna uejnn', 1234566, 1.8, 78, '1992-11-04'),
-(3, 47373772, 'sanchez', 'tomas', 'concaran', 1242134, 1.7, 75, '2022-11-02');
+INSERT INTO `paciente` (`id_paciente`, `dni`, `apellido`, `nombre`, `domicilio`, `telefono`, `altura`, `pesoActual`, `fechaNacimiento`, `estado`) VALUES
+(4, 26912333, 'Lopez Pujato', 'Pablo Javier', 'carmen', 2147483647, 2.05, 75, '0000-00-00', 1),
+(6, 33825432, 'Sanchez', 'Tomas', 'catamarca ', 15245, 1.8, 70, '0000-00-00', 1),
+(8, 44321123, 'Sehitz', 'Jimena', 'catamarca ', 15245, 1.8, 70, '0000-00-00', 1);
 
 --
 -- Índices para tablas volcadas
@@ -104,28 +122,31 @@ INSERT INTO `paciente` (`idPaciente`, `dni`, `apellido`, `nombre`, `domicilio`, 
 -- Indices de la tabla `comida`
 --
 ALTER TABLE `comida`
-  ADD PRIMARY KEY (`idComida`);
+  ADD PRIMARY KEY (`id_comida`),
+  ADD UNIQUE KEY `nombre` (`nombre`) USING BTREE;
 
 --
 -- Indices de la tabla `dieta`
 --
 ALTER TABLE `dieta`
-  ADD PRIMARY KEY (`idDieta`),
-  ADD KEY `idPaciente` (`idPaciente`);
+  ADD PRIMARY KEY (`id_dieta`),
+  ADD UNIQUE KEY `idPaciente_2` (`id_paciente`,`findieta`),
+  ADD KEY `idPaciente` (`id_paciente`);
 
 --
 -- Indices de la tabla `itemcomidas`
 --
 ALTER TABLE `itemcomidas`
-  ADD PRIMARY KEY (`idItemComida`),
-  ADD KEY `idDieta` (`idDieta`),
-  ADD KEY `idComida` (`idComida`);
+  ADD PRIMARY KEY (`id_itemcomida`),
+  ADD UNIQUE KEY `idDieta_2` (`id_dieta`,`id_comida`) USING BTREE,
+  ADD KEY `idDieta` (`id_dieta`),
+  ADD KEY `idComida` (`id_comida`);
 
 --
 -- Indices de la tabla `paciente`
 --
 ALTER TABLE `paciente`
-  ADD PRIMARY KEY (`idPaciente`),
+  ADD PRIMARY KEY (`id_paciente`),
   ADD UNIQUE KEY `dni` (`dni`);
 
 --
@@ -136,42 +157,42 @@ ALTER TABLE `paciente`
 -- AUTO_INCREMENT de la tabla `comida`
 --
 ALTER TABLE `comida`
-  MODIFY `idComida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_comida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `dieta`
 --
 ALTER TABLE `dieta`
-  MODIFY `idDieta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_dieta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `itemcomidas`
 --
 ALTER TABLE `itemcomidas`
-  MODIFY `idItemComida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_itemcomida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `paciente`
 --
 ALTER TABLE `paciente`
-  MODIFY `idPaciente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_paciente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `comida`
---
-ALTER TABLE `comida`
-  ADD CONSTRAINT `comida_ibfk_1` FOREIGN KEY (`idComida`) REFERENCES `itemcomidas` (`idComida`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Filtros para la tabla `dieta`
 --
 ALTER TABLE `dieta`
-  ADD CONSTRAINT `dieta_ibfk_1` FOREIGN KEY (`idPaciente`) REFERENCES `paciente` (`idPaciente`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `dieta_ibfk_2` FOREIGN KEY (`idDieta`) REFERENCES `itemcomidas` (`idDieta`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `dieta_ibfk_1` FOREIGN KEY (`id_paciente`) REFERENCES `paciente` (`id_paciente`);
+
+--
+-- Filtros para la tabla `itemcomidas`
+--
+ALTER TABLE `itemcomidas`
+  ADD CONSTRAINT `itemcomidas_ibfk_1` FOREIGN KEY (`id_dieta`) REFERENCES `dieta` (`id_dieta`),
+  ADD CONSTRAINT `itemcomidas_ibfk_2` FOREIGN KEY (`id_comida`) REFERENCES `comida` (`id_comida`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
