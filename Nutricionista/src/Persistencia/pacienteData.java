365ccd6bb4984986ab166100401c13d538070a78
TommySanchez12 <tomyjoaqo@gmail.com>
2022-11-17 19:49:43 -0300

    PacienteData

--- Nutricionista/src/Persistencia/pacienteData.java
+++ Nutricionista/src/Persistencia/pacienteData.java
@@ -23,7 +23,7 @@ public class pacienteData {
         //INSERT INTO `paciente`(`idPaciente`, `dni`, `apellido`, `nombre`, `domicilio`, `telefono`, `altura`, `pesoActual`, `fechaNacimiento`) 
         //VALUES (?,?,?,?,?,?,?,?,?)
         try {
-            String sql = "INSERT INTO `paciente`( `dni`, `apellido`, `nombre`, `domicilio`, `telefono`, `altura`, `pesoActual`, `fechaNacimiento`) VALUES (?,?,?,?,?,?,?,?)";
+            String sql = "INSERT INTO `paciente`( `dni`, `apellido`, `nombre`, `domicilio`, `telefono`, `altura`, `pesoActual`, `fechaNacimiento`, `estado`) VALUES (?,?,?,?,?,?,?,?,?)";
             PreparedStatement ps = cx.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setLong(1, pa.getDni());
             ps.setString(2, pa.getApellido());
@@ -33,7 +33,17 @@ public class pacienteData {
             ps.setDouble(6, pa.getAltura());
             ps.setDouble(7, pa.getPesoActual());
             ps.setDate(8, java.sql.Date.valueOf(pa.getFechaNacimiento()));
+            ps.setBoolean(9 , true);
             ps.close();
+            int registro = ps.executeUpdate();
+
+            String cartel;
+            if (registro > 0) {
+                cartel = "Paciente agregado";
+            } else {
+                cartel = "No se pudo agregar el paciente";
+            }
+            JOptionPane.showMessageDialog(null, cartel);
         } catch (SQLException ex) {
 
             if (ex.getErrorCode() == 1062) {
