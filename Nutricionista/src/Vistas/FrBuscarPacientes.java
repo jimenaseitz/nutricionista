5c009e021abd3fdca9c2f3659eaf1358a839cb00
TommySanchez12 <tomyjoaqo@gmail.com>
2022-12-09 10:43:39 -0300

    FrMostrarPacientes

--- Nutricionista/src/Vistas/FrBuscarPacientes.java
+++ Nutricionista/src/Vistas/FrBuscarPacientes.java
@@ -81,16 +81,9 @@ public class FrBuscarPacientes extends javax.swing.JInternalFrame {
             Class[] types = new Class [] {
                 java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.Double.class, java.lang.Double.class
             };
-            boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false, false, false
-            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
-            }
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
             }
         });
         jScrollPane1.setViewportView(JtPacientes);
@@ -154,18 +147,17 @@ public class FrBuscarPacientes extends javax.swing.JInternalFrame {
     private void armarTabla() {
 
         ArrayList<Paciente> lista = new ArrayList();
-        Paciente pa = new Paciente();
         pacienteData data = new pacienteData();
         lista = data.buscarpacientes();
         String columnas[] = new String[8];
         DefaultTableModel modelo = (DefaultTableModel) this.JtPacientes.getModel();
         modelo.setNumRows(0);
         for (Paciente aux : lista) {
-            columnas[0] = aux.getNombre() + "";
-            columnas[1] = aux.getApellido()+ "";
+            columnas[0] = aux.getNombre();
+            columnas[1] = aux.getApellido();
             columnas[2] = aux.getDni() + "";
-            columnas[3] = aux.getFechaNacimiento()+ "";
-            columnas[4] = aux.getDomicilio()+ "";
+            columnas[3] = aux.getFechaNacimiento().toString();
+            columnas[4] = aux.getDomicilio();
             columnas[5] = aux.getTelefono()+ "";
             columnas[6] = aux.getPesoActual()+ "";
             columnas[7] = aux.getAltura()+ "";
